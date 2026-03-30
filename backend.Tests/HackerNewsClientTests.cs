78379dd83e1b6572eade9d9117556d5bb8340d11
Jonathan Zuchelli <jrzuchelli@gmail.com>
2026-03-29 23:35:33 -0400

    Needed imports

--- backend.Tests/HackerNewsClientTests.cs
+++ backend.Tests/HackerNewsClientTests.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using HackerNewsAggregator.Models;
 using HackerNewsAggregator.Services;
 
